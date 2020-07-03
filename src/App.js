e8c1bade81dd10f5d9cfc7d5eda748a092db8043
Morgan Sissom <masissom@gmail.com>
2020-07-03 14:59:06 -0400

    Just a lil more coding by numbers

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ const api = {
 }
 
 function App() {
+
+const dateBuilder = (d) => {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+}
+
   return (
     <div className="app">
       <main>
@@ -16,6 +23,10 @@ function App() {
           className='search-bar'
           placeholder="Search..."
           />
+        </div>
+        <div className="location-box">
+        <div className="location">NYC, US</div>
+        <div className="date">{dateBuilder(new Date())}</div>
         </div>
       </main>
     </div>
