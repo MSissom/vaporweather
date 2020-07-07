f7e568d27aed73c68fc7087d584de5cd8e0e4d5e
Morgan Sissom <masissom@gmail.com>
2020-07-07 15:56:02 -0400

    minor additions and styling hopping between work spaces

--- src/App.js
+++ src/App.js
@@ -12,6 +12,13 @@ const dateBuilder = (d) => {
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`
+
 }
 
   return (
@@ -27,6 +34,12 @@ const dateBuilder = (d) => {
         <div className="location-box">
         <div className="location">NYC, US</div>
         <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">
+            80
+          </div>
+          <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
